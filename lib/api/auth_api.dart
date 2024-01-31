e0657bd35f065637ae608612960d96db7f9d0422
fikrilal <fikrimuzakky24@gmail.com>
2024-01-31 09:41:18 +0700

    base url

--- lib/api/auth_api.dart
+++ lib/api/auth_api.dart
@@ -3,12 +3,13 @@ import 'dart:convert';
 import 'package:permata_architect_mobile_apps/models/auth_model/user_model.dart';
 import 'package:http/http.dart' as http;
 
+import '../repository/api/api_config.dart';
+
 class AuthService {
-  String baseUrl = 'http://192.168.0.105:8000/api';
 
   Future<UserModel> register(
       {String? name, String? email, String? password}) async {
-    Uri url = Uri.parse('$baseUrl/register');
+    Uri url = Uri.parse('${ApiConfig.baseUrl}/register');
     var body = jsonEncode({'name': name, 'email': email, 'password': password});
     var headers = {'Content-Type': 'application/json'};
     var response = await http.post(url, headers: headers, body: body);
@@ -27,7 +28,7 @@ class AuthService {
   }
 
   Future<UserModel> login({String? email, String? password}) async {
-    Uri url = Uri.parse('$baseUrl/login');
+    Uri url = Uri.parse('${ApiConfig.baseUrl}/login');
     var body = jsonEncode({'email': email, 'password': password});
     var headers = {'Content-Type': 'application/json'};
     var response = await http.post(url, headers: headers, body: body);
